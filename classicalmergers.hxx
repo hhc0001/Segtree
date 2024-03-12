353ee303c042f31138e860cfc1596ac5449c5786
Chinese:Haichuan He, English: Rander Henry <122586746+hhc0001@users.noreply.github.com>
2024-03-12 16:46:24 +0800

    Ok, it finished.

--- classicalmergers.hxx
+++ classicalmergers.hxx
@@ -1,4 +1,69 @@
-
+#include <algorithm>
 
 namespace expand {
+  #ifndef MERGERS
+  #define MERGERS 1
+  template<
+  typename _Tp
+  >struct add {
+    _Tp operator ()(const _Tp &a, const _Tp &b) {
+      return a + b;
+    }
+  };
+  template<
+  typename _Tp,
+  long long mod
+  >struct addmod {
+    _Tp operator ()(const _Tp &a, const _Tp &b) {
+      return (a + b) % mod;
+    }
+  };
+
+  template<
+  typename _Tp
+  >struct mult {
+    _Tp operator ()(const _Tp &a, const _Tp &b) {
+      return a * b;
+    }
+  };
+  template<
+  typename _Tp,
+  long long mod
+  >struct multmod {
+    _Tp operator ()(const _Tp &a, const _Tp &b) {
+      return a * b % mod;
+    }
+  };
+
+  template<
+  typename _Tp
+  >struct maximum {
+    _Tp operator ()(const _Tp &a, const _Tp &b) {
+      return max(a, b);
+    }
+  };
+  template<
+  typename _Tp
+  >struct minimum {
+    _Tp operator ()(const _Tp &a, const _Tp &b) {
+      return min(a, b);
+    }
+  };
+
+  template<
+  typename _Tp
+  >struct gcd {
+    _Tp operator ()(const _Tp &a, const _Tp &b) {
+      return !b? a : operator()(b, a % b);
+    }
+  };
+  template<
+  typename _Tp
+  >struct lcm {
+    _Tp operator ()(const _Tp &a, const _Tp &b) {
+      gcd<_Tp> g;
+      return a / g(a, b) * b;
+    }
+  };
+  #endif // MERGERS
 }
